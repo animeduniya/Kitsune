371bcee50f92e30dce92fc39ea9d08a5c8158794
animeduniya <duniyabarbaad24@gmail.com>
2025-02-23 17:50:31 +0530

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://aniwatch-api-beta-ten.vercel.app'
+    baseURL: 'https://aniwatch-api-beta-ten.vercel.app/v2/hianime'
 })
